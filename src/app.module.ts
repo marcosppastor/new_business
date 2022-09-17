a6b52c8ef04cc4dbed04b4888c9ac348257096f1
Marcos Paulo Pastor <marcosppastor@gmail.com>
2022-09-17 19:38:59 -0300

    Criação da resource de 'users' e 'plans' nos módulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,16 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleModule } from '@nestjs/schedule';
 import { databaseConfig } from './configs/database';
+import { UsersModule } from './modules/users/users.module';
+import { PlansModule } from './modules/plans/plans.module';
 
 @Module({
     imports: [
         ConfigModule.forRoot(),
         ScheduleModule.forRoot(),
         TypeOrmModule.forRoot(databaseConfig),
+        UsersModule,
+        PlansModule,
     ],
     controllers: [
         //
